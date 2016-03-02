fb7ae75a0ca161b7149084c1c507f0c9ad3a1394
Teun <teun.willems@live.nl>
2016-03-02 12:58:36 +0100

    Initial commit

---
+++ src/main/java/nl/dickbutt/DickbuttApplication.java
@@ -0,0 +1,12 @@
+package nl.dickbutt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DickbuttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DickbuttApplication.class, args);
+	}
+}
