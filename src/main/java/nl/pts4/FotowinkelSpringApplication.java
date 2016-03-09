5a1c4776e091e599c19d419702f956123c61e9cb
Teun <teun.willems@live.nl>
2016-03-09 08:49:47 +0100

    Spring finally working!

--- src/main/java/nl/pts4/FotowinkelSpringApplication.java
+++ src/main/java/nl/pts4/FotowinkelSpringApplication.java
@@ -1,12 +1,12 @@
-package nl.dickbutt;
+package nl.pts4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DickbuttApplication {
+public class FotowinkelSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DickbuttApplication.class, args);
+		SpringApplication.run(FotowinkelSpringApplication.class, args);
 	}
 }
