29d335e8896961b1fa921c26f8285ea01431398c
Teun <teun.willems@live.nl>
2016-03-16 10:32:17 +0100

    Removed target

---
+++ src/main/java/nl/pts4/Account.java
@@ -0,0 +1,7 @@
+package nl.pts4;
+
+/**
+ * Created by Teun on 16-3-2016.
+ */
+public class Account {
+}
--- src/main/java/nl/pts4/authentication/GoogleOAut.java
+++
@@ -1,7 +0,0 @@
-package nl.pts4.authentication;
-
-/**
- * Created by Teun on 9-3-2016.
- */
-public class GoogleOAut {
-}
