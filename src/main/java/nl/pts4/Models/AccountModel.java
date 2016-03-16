d3a2d9975e6a702b0d53c36d4a88be4039a12f7b
Guus Hamm <guushamm@gmail.com>
2016-03-16 10:18:18 +0100

    Added Account Model

---
+++ src/main/java/nl/pts4/Models/AccountModel.java
@@ -0,0 +1,59 @@
+package nl.pts4.Models;
+
+/**
+ * Created by GuusHamm on 16-3-2016.
+ */
+public class AccountModel {
+	private int id;
+	private String oAuthKey;
+	private OAuhtProviderEnum oAuhtProvider;
+	private String name;
+	private String email;
+	private String hash;
+	private String salt;
+	private boolean active;
+	private AccountTypeEnum accountTypeEnum;
+
+	public enum AccountTypeEnum {
+		customer,
+		photographer
+	}
+
+	public enum OAuhtProviderEnum {
+		google,
+		facebook,
+		twitter
+	}
+
+	public AccountModel(int id, String oAuthKey, OAuhtProviderEnum oAuhtProvider, String name, String email, String hash, String salt, boolean active, AccountTypeEnum accountTypeEnum) {
+		this.id = id;
+		this.oAuthKey = oAuthKey;
+		this.oAuhtProvider = oAuhtProvider;
+		this.name = name;
+		this.email = email;
+		this.hash = hash;
+		this.salt = salt;
+		this.active = active;
+		this.accountTypeEnum = accountTypeEnum;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public AccountTypeEnum getAccountTypeEnum() {
+		return accountTypeEnum;
+	}
+}
