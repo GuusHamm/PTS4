27f27bcac18263b677d78c7aad999a347baf3a23
Wouter Vanmulken <woutervanmulken@msn.com>
2016-03-23 13:06:54 +0100

    added multilanguage support, there still needs to be a button that does this.

--- src/main/java/nl/pts4/GreetingController.java
+++ src/main/java/nl/pts4/GreetingController.java
@@ -1,9 +1,12 @@
 package nl.pts4;
 
+import org.apache.commons.lang.LocaleUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Locale;
 
 /**
  * Created by Teun on 9-3-2016.
@@ -15,6 +18,10 @@ public class GreetingController {
     public String test(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "test";
+    }
+    @RequestMapping("/languages")
+    public String languages(@RequestParam(value="language", required=false, defaultValue="World") String language, Model model) {
+        return "WelcomePage";
     }
 
 }
