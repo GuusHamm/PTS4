50e88170b0c6d66e6f79a52cb15ed8da4f6f8f7c
Teun <teun.willems@live.nl>
2016-03-30 12:50:09 +0200

    Fixed links and login form

--- src/main/java/nl/pts4/model/AccountRestModel.java
+++ src/main/java/nl/pts4/model/AccountRestModel.java
@@ -2,6 +2,9 @@ package nl.pts4.model;
 
 import nl.pts4.controller.AccountController;
 import nl.pts4.controller.DatabaseController;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
 
 /**
  * Created by Teun on 16-3-2016.
@@ -11,14 +14,14 @@ public class AccountRestModel {
     private boolean success;
     private String message = "No message specified";
 
-    public AccountRestModel(String email, String password) {
+    public AccountRestModel(String email, String password, Locale locale, MessageSource messageSource) {
         AccountModel accountModel = DatabaseController.getInstance().getAccount(email);
         boolean correct = AccountController.checkPassword(accountModel, password);
         if (!correct) {
-            message = "Incorrect username or password";
+            message = messageSource.getMessage("error.authentication", null, locale);
         }
         else {
-            message = "Succesful login";
+            message = messageSource.getMessage("login.success", null, locale);
             success = true;
         }
     }
