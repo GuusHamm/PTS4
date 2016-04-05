ea0942d633ef55fb4cd6c6ca0581912e28b32e60
guushamm <guushamm@gmail.com>
2016-04-05 17:47:50 +0200

    Optimized userIsPrivileged to make use of session to store the result to increase performance

--- src/main/java/nl/pts4/controller/MainController.java
+++ src/main/java/nl/pts4/controller/MainController.java
@@ -16,54 +16,75 @@ import java.io.IOException;
 @Controller
 public class MainController {
 
-    public static final String TITLE_ATTRIBUTE = "title";
-    public static final String ERROR_ATTRIBUTE = "error";
-    public static final String SUCCESS_ATTRIBUTE = "success";
+	public static final String TITLE_ATTRIBUTE = "title";
+	public static final String WARNING_ATTRIBUTE = "warning";
+	public static final String ERROR_ATTRIBUTE = "error";
+	public static final String SUCCESS_ATTRIBUTE = "success";
+	public static final String PRIVILEGED_ATTRIBUTE = "privileged";
 
-    public static boolean assertUserIsPrivileged(String account, HttpServletRequest request, HttpServletResponse response) {
-        if (account == null || !DatabaseController.getInstance().checkPrivalegedUser(DatabaseController.getInstance().getAccountByCookie(account).getUuid())) {
-            try {
-                request.getSession().setAttribute(MainController.ERROR_ATTRIBUTE, "You are not allowed to do that");
-                response.sendRedirect("/login");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return false;
-        }
-        return true;
-    }
+	public static boolean assertUserIsPrivileged(String account, HttpServletRequest request, HttpServletResponse response, boolean redirect) {
+		Object privilegedAttribute = request.getSession().getAttribute(PRIVILEGED_ATTRIBUTE);
 
-    /**
-     * The main page
-     * @param m         : The model / template
-     * @param account   : The account cookie, is not required
-     * @return main to load the correct template
-     */
-    @RequestMapping(value = "/")
-    public String main(Model m, @CookieValue(value = AccountController.AccountCookie, required = false) String account, HttpServletRequest request) {
-        m.addAttribute(MainController.TITLE_ATTRIBUTE, "Fotowinkel");
-        AccountModel am = DatabaseController.getInstance().getAccountByCookie(account);
-        m.addAttribute("user", am);
-        m.addAttribute("cart", request.getSession().getAttribute("Cart"));
-        if (account == null) {
-            m.addAttribute("privileged", false);
-        } else {
-            m.addAttribute("privileged", DatabaseController.getInstance().checkPrivalegedUser(DatabaseController.getInstance().getAccountByCookie(account).getUuid()));
-        }
-        return "main";
-    }
+		if (account == null) {
+			if (redirect) {
+				redirectToLogin(request, response);
+			}
+			return false;
+		}
 
-    /**
-     * The header of the page
-     * @param m         : The model / template
-     * @param account   : The account cookie, is not required
-     * @return header to get the correct template
-     */
-    @RequestMapping(value = "/header")
-    public String header(Model m, @CookieValue(value = AccountController.AccountCookie, required = false) String account) {
-        m.addAttribute(MainController.TITLE_ATTRIBUTE, "Fotowinkel");
-        AccountModel am = DatabaseController.getInstance().getAccountByCookie(account);
-        m.addAttribute("user", am);
-        return "header";
-    }
+		if (privilegedAttribute == null) {
+			privilegedAttribute = DatabaseController.getInstance().checkPrivalegedUser(DatabaseController.getInstance().getAccountByCookie(account).getUuid());
+			request.getSession().setAttribute(PRIVILEGED_ATTRIBUTE, privilegedAttribute);
+		} else {
+			privilegedAttribute = request.getSession().getAttribute(PRIVILEGED_ATTRIBUTE);
+		}
+
+		if ((boolean) privilegedAttribute == false) {
+			if (redirect) {
+				redirectToLogin(request, response);
+			}
+		}
+		return (boolean) privilegedAttribute;
+	}
+
+	private static void redirectToLogin(HttpServletRequest request, HttpServletResponse response) {
+		request.getSession().setAttribute(MainController.ERROR_ATTRIBUTE, "You are not allowed to do that");
+		try {
+			response.sendRedirect("/login");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * The main page
+	 *
+	 * @param m       : The model / template
+	 * @param account : The account cookie, is not required
+	 * @return main to load the correct template
+	 */
+	@RequestMapping(value = "/")
+	public String main(Model m, @CookieValue(value = AccountController.AccountCookie, required = false) String account, HttpServletRequest request, HttpServletResponse response) {
+		m.addAttribute(MainController.TITLE_ATTRIBUTE, "Fotowinkel");
+		AccountModel am = DatabaseController.getInstance().getAccountByCookie(account);
+		m.addAttribute("user", am);
+		m.addAttribute("cart", request.getSession().getAttribute("Cart"));
+		m.addAttribute(MainController.PRIVILEGED_ATTRIBUTE, MainController.assertUserIsPrivileged(account, request, response, false));
+		return "main";
+	}
+
+	/**
+	 * The header of the page
+	 *
+	 * @param m       : The model / template
+	 * @param account : The account cookie, is not required
+	 * @return header to get the correct template
+	 */
+	@RequestMapping(value = "/header")
+	public String header(Model m, @CookieValue(value = AccountController.AccountCookie, required = false) String account) {
+		m.addAttribute(MainController.TITLE_ATTRIBUTE, "Fotowinkel");
+		AccountModel am = DatabaseController.getInstance().getAccountByCookie(account);
+		m.addAttribute("user", am);
+		return "header";
+	}
 }
