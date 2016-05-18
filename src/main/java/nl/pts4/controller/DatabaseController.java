4d3568c21d2aae48b8d122dbba7aa7e7abdae63b
Guus Hamm <guushamm@gmail.com>
2016-05-18 10:15:52 +0200

    You can now only view your own photo's and some visual tweaks

--- src/main/java/nl/pts4/controller/DatabaseController.java
+++ src/main/java/nl/pts4/controller/DatabaseController.java
@@ -329,6 +329,42 @@ public class DatabaseController {
 //        return orderModels;
 //    }
 
+
+    /**
+     * Get all the orders of a account from the database
+     *
+     * @return a list of order models from the database
+     */
+    public List<OrderModel> getAllAccountOrders(UUID uuid) {
+        JdbcTemplate template = new JdbcTemplate(dataSource);
+
+        List<Map<String, Object>> rows = template.queryForList("SELECT o.id, o.accountid, o.orderdate FROM order_ o WHERE accountid = ?",new Object[]{uuid});
+        HashMap<UUID, AccountModel> accountModels = getAllAccounts();
+        List<OrderModel> orderModels = new ArrayList<>(rows.size());
+        for (Map<String, Object> row : rows) {
+            int id = (int) row.get("id");
+            UUID account = (UUID) row.get("accountid");
+            Date orderDate = (Date) row.get("orderdate");
+
+            orderModels.add(new OrderModel(id, orderDate, accountModels.get(account)));
+        }
+
+        rows = template.queryForList("SELECT o.id, o.orderid, o.photoconfigurationid FROM orderline o");
+        for (Map<String, Object> row : rows) {
+            int id = (int) row.get("id");
+            int orderid = (int) row.get("orderid");
+            int photoConfigurationId = (int) row.get("photoconfigurationid");
+            OrderLineModel olm = new OrderLineModel(id, orderid, photoConfigurationId);
+
+            Optional<OrderModel> o = orderModels.stream().filter(a -> a.getId() == orderid).findFirst();
+            if (o.isPresent()) {
+                o.get().getOrderLineModels().add(olm);
+            }
+        }
+
+        return orderModels;
+    }
+
     /**
      * Get all the orders from the database
      *
