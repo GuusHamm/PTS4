b21622f89a934388a67ea16f708e5be12265113a
Teun <teun.willems@live.nl>
2016-06-08 10:48:37 +0200

    Added QR code "id/create"

---
+++ src/main/java/nl/pts4/model/QRIdentification.java
@@ -0,0 +1,15 @@
+package nl.pts4.model;
+
+/**
+ * Created by Teun on 8-6-2016.
+ */
+public class QRIdentification extends IdentificationModel {
+
+    private String link;
+
+    public QRIdentification(String link) {
+        super();
+        this.link = link;
+    }
+
+}
